cf54f9d9f4fa6443c94481570324a493b76adabb
Navya Gaddam <137303754+Navya-Gaddam@users.noreply.github.com>
2023-09-29 20:18:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,49 @@
 //your code here!
+const scrollContainer = document.getElementById("infi-list");
+
+scrollContainer.addEventListener("scroll", () => {
+	const scrollTop = scrollContainer.scrollTop;
+	const containerHeight = scrollContainer.clientHeight;
+	const scrollHeight = scrollContainer.scrollHeight;
+	const maximumScrollableHeight = scrollHeight - containerHeight;
+
+	const threshold = Math.abs((scrollTop) - maximumScrollableHeight);
+
+	if(threshold <= 5){
+		const div = document.createElement("div");
+		div.innerHTML = `
+		  <li>Item 1</li>
+		  <li>Item 2</li>
+		  <li>Item 3</li>
+		  <li>Item 4</li>
+		  <li>Item 5</li>
+		  <li>Item 6</li>
+		  <li>Item 7</li>
+		  <li>Item 8</li>
+		  <li>Item 9</li>
+		  <li>Item 10</li>
+		  <li>Item 11</li>
+		  <li>Item 12</li>
+		  <li>Item 13</li>
+		  <li>Item 14</li>
+		  <li>Item 15</li>
+		  <li>Item 1</li>
+		  <li>Item 2</li>
+		  <li>Item 3</li>
+		  <li>Item 4</li>
+		  <li>Item 5</li>
+		  <li>Item 6</li>
+		  <li>Item 7</li>
+		  <li>Item 8</li>
+		  <li>Item 9</li>
+		  <li>Item 10</li>
+		  <li>Item 11</li>
+		  <li>Item 12</li>
+		  <li>Item 13</li>
+		  <li>Item 14</li>
+		  <li>Item 15</li>`;
+		scrollContainer.appendChild(div);
+	}
+});
+
 
